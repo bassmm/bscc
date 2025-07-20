f243f0bad6626de2de73fa107a072e2c93b88301
Bassam <bassamtop123@gmail.com>
2025-07-20 17:15:47 +0300

    Add shopping, transport, and vehicle categories with relevant entries
    - Created shopping.txt with 657 entries including popular retailers and e-commerce platforms.
    - Created transport.txt with 764 entries covering various transport services and modes.
    - Created vehicle.txt with 757 entries related to fuel stations, insurance providers, and vehicle services.
    - Updated uv.lock to include dependencies for the project, specifying versions for packages like pandas, numpy, and scikit-learn.

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_files
+
+def load_data(file_containing_data: str) -> tuple:
+    dataset = load_files(file_containing_data, encoding='utf-8', shuffle=False)
+    texts = dataset.get('data')
+    labels = dataset.get('target_names')
+    return (texts, labels)
+
 def main():
-    print("Hello from bscc!")
+
+    texts, labels = load_data("categories")
+
+    vectorizer = TfidfVectorizer()
+    text_vectors = vectorizer.fit_transform(texts)
+
+    # Train classifier
+    clf = LogisticRegression()
+    clf.fit(text_vectors, labels)
+
+    # Predict new string
+    new_text = [""]
+    new_text_vector = vectorizer.transform(new_text)
+    print(clf.predict(new_text_vector))
+    for label in labels:
+        print(f"{label.capitalize()}: {clf.score(new_text_vector, [label])}")
+
 
 
 if __name__ == "__main__":
